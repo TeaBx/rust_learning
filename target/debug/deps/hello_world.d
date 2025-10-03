b6c755315c867208222aad544f21de47ba1e0c58
Teodora.B <btea65952@gmail.com>
2025-10-03 12:51:08 +0200

    Initial commit: Hello World program

---
+++ target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\TeodoraBojanic\rust_projects\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\Users\TeodoraBojanic\rust_projects\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
